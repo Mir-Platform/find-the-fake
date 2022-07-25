4dbce5c45b17d58eec6700ed5d9b77bd8816f76b
timmson <timmson666@mail.ru>
2022-07-25 18:00:11 +0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import "bootstrap"
+import "./index.scss"
+
+import React from "react"
+import {createRoot} from "react-dom/client"
+import App from "./app"
+
+createRoot(document.getElementById("app")!).render(App)
