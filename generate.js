e0a32ddb82a9820c4ac80a3704fbf0c026ae33fa
timmson <timmson666@mail.ru>
2022-08-22 17:08:51 +0300

    Add new design

--- generate.js
+++ generate.js
@@ -4,15 +4,26 @@ const configFile = "./src/files.tsx"
 const webDir = "./docs/"
 const realDir = "./img/real/"
 const fakeDir = "./img/fake/"
+const testFiles = ["./docs/img/real/real.jpg", "./docs/img/fake/fake.jpg"]
 
 console.log("Started")
 
+console.log("Removing test files...")
+testFiles.forEach(f => fs.unlink(f, (err) => {
+		if (err) {
+			throw err
+		}
+		console.log(f + " was deleted")
+	}
+))
+
+console.log("Generate index...")
 const files = {
 	real: fs.readdirSync(webDir + realDir).map((file) => realDir + file),
-	fake: fs.readdirSync(webDir+ fakeDir).map((file) => fakeDir + file)
+	fake: fs.readdirSync(webDir + fakeDir).map((file) => fakeDir + file)
 }
 
-fs.writeFile(configFile, "const files = " + JSON.stringify(files) +"\nexport default files", (err) => {
+fs.writeFile(configFile, "const files = " + JSON.stringify(files) + "\nexport default files", (err) => {
 	if (err) {
 		throw err
 	}
